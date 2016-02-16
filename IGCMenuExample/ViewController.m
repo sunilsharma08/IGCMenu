2c50d469ae56a1c1ba56e34f31567486059d2279
Sunil Sharma <sunilsharma.ss08@gmail.com>
2016-02-16 16:26:04 +0530

    Customisation property commented

--- IGCMenuExample/ViewController.m
+++ IGCMenuExample/ViewController.m
@@ -31,21 +31,34 @@
     if (igcMenu == nil) {
         igcMenu = [[IGCMenu alloc] init];
     }
-    igcMenu.disableBackground = YES;
-    igcMenu.numberOfMenuItem = 5;
-    igcMenu.menuButton = self.menuButton;
-    igcMenu.menuSuperView = self.view;
+    igcMenu.menuButton = self.menuButton;   //Pass refernce of menu button
+    igcMenu.menuSuperView = self.view;      //Pass reference of menu button super view
+    igcMenu.disableBackground = YES;        //Enable/disable menu background
+    igcMenu.numberOfMenuItem = 5;           //Number of menu items to display
+    
+    /* Optional
+     Pass name of menu items
+     **/
     igcMenu.menuItemsNameArray = [NSArray arrayWithObjects:@"Home",@"Like",@"Search",@"User",@"Buy",nil];
     
+    /*Optional
+     Pass color of menu items
+     **/
     UIColor *homeBackgroundColor = [UIColor colorWithRed:(33/255.0) green:(180/255.0) blue:(227/255.0) alpha:1.0];
     UIColor *searchBackgroundColor = [UIColor colorWithRed:(139/255.0) green:(116/255.0) blue:(240/255.0) alpha:1.0];
     UIColor *favoritesBackgroundColor = [UIColor colorWithRed:(241/255.0) green:(118/255.0) blue:(121/255.0) alpha:1.0];
     UIColor *userBackgroundColor = [UIColor colorWithRed:(184/255.0) green:(204/255.0) blue:(207/255.0) alpha:1.0];
     UIColor *buyBackgroundColor = [UIColor colorWithRed:(169/255.0) green:(59/255.0) blue:(188/255.0) alpha:1.0];
-    
     igcMenu.menuBackgroundColorsArray = [NSArray arrayWithObjects:homeBackgroundColor,favoritesBackgroundColor,searchBackgroundColor,userBackgroundColor, buyBackgroundColor,nil];
+    
+    /*Optional
+     Pass menu items icons
+     **/
     igcMenu.menuImagesNameArray = [NSArray arrayWithObjects:@"home.png",@"favourite.png",@"search.png",@"user.png",@"buy.png",nil];
     
+    /*Optional if you don't want to get notify for menu items selection
+     conform delegate
+     **/
     igcMenu.delegate = self;
 }
 - (IBAction)menuButtonPressed:(id)sender {
